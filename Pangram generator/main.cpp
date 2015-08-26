79833354de923167725f6a9940061ca40c850f02
wheat779 <legendarycake@gmail.com>
2015-08-25 23:59:14 -0700

    Initial Commit

---
+++ Pangram generator/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Pangram generator
+//
+//  Created by Cameron Kemper on 8/25/15.
+//  Copyright (c) 2015 Cameron Kemper. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
